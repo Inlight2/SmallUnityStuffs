c21491707d8f02088974b8443b65779e839232fe
inlight2 <kennethCox5@gmail.com>
2015-11-29 13:43:31 -0800

    a project to simulate a ball being thrown between any two points with a proper gravity arc

---
+++ thrownArc/Assets/ArcMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcMovement : MonoBehaviour {
+
+	[SerializeField] float initialVerticalVelocity = 0.5f;
+	[SerializeField] float horizontalDistance = 30;
+	[SerializeField] float verticalOffset = 0;
+
+	[SerializeField] const float GRAVITY = 0.01f;
+
+	void Awake () {
+		StartCoroutine (ArcMoveUpdate ());
+	}
+
+	IEnumerator ArcMoveUpdate() {
+		float originY = transform.localPosition.y;
+		float targetY = originY + verticalOffset;
+		int gravTicks = 0;
+		float verticalVelocity = initialVerticalVelocity;
+
+		for (float y = originY; y > targetY || verticalVelocity >= 0; gravTicks++) {
+			y += verticalVelocity;
+			verticalVelocity -= GRAVITY;
+		}
+
+		verticalVelocity = initialVerticalVelocity;
+		float horizontalSpeed = (float) horizontalDistance / gravTicks;
+		while (transform.localPosition.y > targetY || verticalVelocity >= 0) {
+			transform.localPosition = new Vector3 (transform.localPosition.x + horizontalSpeed,
+			                                       transform.localPosition.y + verticalVelocity,
+			                                       transform.localPosition.z);
+			verticalVelocity -= GRAVITY;
+
+			yield return null;
+		}
+	}
+}
