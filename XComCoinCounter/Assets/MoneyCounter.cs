113d1b4459a09ecd5001c115e23595d15884b214
inlight2 <kennethCox5@gmail.com>
2015-12-02 00:01:18 -0800

    ui set up ready for code

--- XComCoinCounter/Assets/MoneyCounter.cs
+++ XComCoinCounter/Assets/MoneyCounter.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MoneyCounter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	[SerializeField] Button TakeResButton;
+	[SerializeField] Button PutBackResButton;
+
+	[SerializeField] Button AddMaxButton;
+	[SerializeField] Button SubMaxButton;
+	[SerializeField] Button ClearAllButton;
+
+	const int INIT_RESERVES = 10;
+	const int INIT_MAX = 10;
+
+	int unassignedPool;
+	int reserve;
+
+	void Start() {
+		unassignedPool = INIT_MAX;
+		reserve = INIT_RESERVES;
 	}
 }
