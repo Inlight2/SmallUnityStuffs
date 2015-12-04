6180032467ebbe1a721ce2bfb862cce5b896c341
inlight2 <kennethCox5@gmail.com>
2015-12-03 17:05:03 -0800

    added logic for main features, buttons don't trigger

--- XComCoinCounter/Assets/MoneyCounter.cs
+++ XComCoinCounter/Assets/MoneyCounter.cs
@@ -1,24 +1,78 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MoneyCounter : MonoBehaviour {
 
-	[SerializeField] Button TakeResButton;
-	[SerializeField] Button PutBackResButton;
+	[SerializeField] GameObject coinPrefab;
 
-	[SerializeField] Button AddMaxButton;
-	[SerializeField] Button SubMaxButton;
-	[SerializeField] Button ClearAllButton;
+	[SerializeField] Button takeResButton;
+	[SerializeField] Button putBackResButton;
+
+	[SerializeField] Button addMaxButton;
+	[SerializeField] Button subMaxButton;
+	[SerializeField] Button clearAllButton;
+
+	public static MoneyCounter masterCounter;
 
 	const int INIT_RESERVES = 10;
 	const int INIT_MAX = 10;
 
+	const int X_SPACING = 36;
+	const int Y_SPACING = 30;
+
 	int unassignedPool;
 	int reserve;
 
+	List<GameObject> coins = new List<GameObject>();
+
 	void Start() {
+		masterCounter = this;
 		unassignedPool = INIT_MAX;
 		reserve = INIT_RESERVES;
+		while (!UpdateVisual()) {}
+	}
+
+	bool UpdateVisual(){
+		if (coins.Count > reserve) {
+			GameObject coin = coins [coins.Count - 1];
+			coins.Remove (coin);
+			Destroy (coin);
+		} else if (coins.Count < reserve) {
+			GameObject coin = Instantiate<GameObject> (coinPrefab);
+			coins.Add (coin);
+			int y = (int)Mathf.Ceil (reserve / 10);
+			int x = (reserve - 1) % 10;
+			x++;
+			coin.transform.localPosition = new Vector3 (X_SPACING * x, Y_SPACING * y, 0);
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	/// <summary>
+	/// Takes a coin from the master Counter.
+	/// </summary>
+	/// <returns><c>true</c>, if coin was taken, <c>false</c> otherwise.</returns>
+	public bool TakeCoin(){
+		if (reserve > 0) {
+			reserve--;
+			UpdateVisual();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// Gives a Coin to the master counter
+	/// </summary>
+	/// <returns><c>true</c>, if coin was given, <c>false</c> otherwise.</returns>
+	public bool GiveCoin(){
+		reserve ++;
+		UpdateVisual ();
+		return true;
 	}
 }
---
+++ XComCoinCounter/Assets/UsedCoins.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UsedCoins : MonoBehaviour {
+
+	[SerializeField] GameObject originCoin;
+
+	[SerializeField] Button takeCoinButton;
+	[SerializeField] Button returnCoinButton;
+
+	int coinCount = 0;
+
+	const int X_DELTA = 10;
+	const int Y_DELTA = 10;
+
+	List<GameObject> coins = new List<GameObject>();
+
+	void Start() {
+		takeCoinButton.onClick.AddListener(delegate{TakeCoin();});
+		returnCoinButton.onClick.AddListener(delegate {ReturnCoin ();});
+	}
+
+	void TakeCoin() {
+		if (MoneyCounter.masterCounter.TakeCoin ()) {
+			coinCount ++;
+			int x = (coinCount-1) % 2;
+			int y = (int)Mathf.Floor( coinCount / 2);
+			GameObject coin = Instantiate<GameObject>(originCoin);
+			coin.transform.localPosition = new Vector3(originCoin.transform.localPosition.x + x * X_DELTA,
+			                                           originCoin.transform.localPosition.y + y * Y_DELTA,
+			                                           originCoin.transform.localPosition.z);
+			coins.Add(coin);
+		}
+	}
+
+	void ReturnCoin() {
+		if (coinCount > 0) {
+			MoneyCounter.masterCounter.GiveCoin ();
+			coinCount --;
+			GameObject coin = coins[coins.Count -1];
+			coins.Remove(coin);
+			Destroy(coin);
+		}
+	}
+}
